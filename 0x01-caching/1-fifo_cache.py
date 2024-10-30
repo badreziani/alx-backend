801870ae74d87cb94c3642a51cf057f01800305c
Badre Ziani <badreziani@gmail.com>
2024-10-30 13:50:50 +0100

    Update task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,35 +2,30 @@
 """
 1-fifo_cache module
 """
-
-
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """Inherits from `BaseCaching` and is a caching system.
+    """Inherits from BaseCaching and is a caching system
     """
-
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """assign to the dictionary `self.cache_data` the
-           `item` value for the key `key`
+        """Adds an item in the cache.
         """
-
         if key is None or item is None:
             return
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
-
         self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
-        """return the value in `self.cache_data` linked to `key`
+        """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
